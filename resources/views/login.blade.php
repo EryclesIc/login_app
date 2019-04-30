31e175d112d0f020ecb1cc68b0587d57af850719
erycles.santos <ess2@ic.ufal.br>
2019-04-30 13:54:38 -0300

    tela de login com materialize

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,45 +2,45 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
-		<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-		<link href="{{ asset('css/login.css') }}" rel="stylesheet">
+		{{-- <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css"> --}}
 		<!-- Compiled and minified CSS -->
 		<link rel="stylesheet" href="css/materialize.css">
+		<link href="{{ asset('css/custon.css') }}" rel="stylesheet">
 		<title>Sistema de Postos</title>
 	</head>
 	<body>
-		<div>
-			<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" style="background-color: black !important;" id="mainNav">
-				<a class="navbar-brand js-scroll-trigger" href="#page-top"><img src="../images/logo_navbar.png" height="60" width="450" style="margin-left: 10%;"/></a>
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				</button>			
-			</nav>
-			<div class="container">
-				<div class ="row">
-					<div class="col">
-						<div class="card" style="height: 21rem; width: 21rem;">
-							<h1 class="card-header">Login</h1>
-							<div class="card-body">
-								<form method="POST" action="/login">
-									<input type="hidden" name="_token" value="{{ csrf_token() }}"><br>
 
-									<div class="login_inputs">
-										<p><input type="text" class="form-control" name="cpf" placeholder="CPF"></p>
-
-										<p><input type="password" class="form-control" name="password" placeholder="Senha"></p>
-										<label>Não tem cadastro,</label><a href='/register'>Registre-se</a>
-									</div><br>
-
-									<input type="submit" class="btn btn-primary" id="btn-login" value="Log In">
-
-								</form>
-							</div>
+		<div class="card-panel black z-depth-2 fixed-top" id="mainNav">
+	
+		</div>
+	
+		<div class="container">
+			<div class ="row">
+				<div class="col s12 m6">
+					<div class="card">
+						<div class="card-content z-depth-2">
+							<span class="card-title"><b>Acesso ao Sistema</b></span>
 						</div>
+						<div class="card-action z-depth-2">
+							<form method="POST" action="/login">
+								<input type="hidden" name="_token" value="{{ csrf_token() }}"><br>
+
+								<div class="login_inputs">
+									<p><input type="text" class="form-control" name="cpf" placeholder="CPF"></p>
+
+									<p><input type="password" class="form-control" name="password" placeholder="Senha"></p>
+									<label>Esqueceu sua senha?  </label><a href='/register'>Recuperar Senha</a>
+								</div><br>
+
+								<input type="submit" class="btn btn-primary" id="btn-login" value="Acessar">
+
+							</form>
+						  </div>
 					</div>
 				</div>
 			</div>
 		</div>
+
 		{{-- <script src = "https://code.jquery.com/jquery-1.12.0.min.js"></script> --}}
 		<!-- Compiled and minified JavaScript -->
 		{{-- <script src="js/materialize.min.js"></script> --}}
